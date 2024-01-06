69b38f671a0d10e04c6cb642899e291f0dcbe7fa
Wilson Maximo <wilson-max@hotmail.com>
2024-01-06 13:46:15 -0300

    Começando a criar o portifolio - Sobre mim

---
+++ portifolio/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="headerClass">
+        <button className="btn btn-projects">Projects</button>
+        <button className="btn btn-resume">Skills</button>
+        <button className="btn btn-about">About</button>
+        <button className="btn btn-contact">Contact</button>
+      </header>
+
+      <body>
+        <p>Olá, meu nome é Wilson.
+          Sou estudante desevolvimento web full stack, tendo experiencia com projetos academicos bastante desafiadores e certificados tanto de front, como de back-end.
+          Sempre lidei bem com os desafios que a tecnologia nos apresente dia após dia. Tendo facilidade com o aprendizado de novos recursos, ferramentas e habilidades de soft skills, estou preparado para ingressar nessa carreira e contribuir com o crescimento da empresa que irei trabalhar.
+        </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
